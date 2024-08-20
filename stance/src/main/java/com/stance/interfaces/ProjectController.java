603a0185f00b2b77822ee4f3030a6ec64696f95c
gangfunction <gangfunction@gmail.com>
2024-08-20 14:22:43 +0900

    Update ProjectController.java

--- stance/src/main/java/com/stance/interfaces/ProjectController.java
+++ stance/src/main/java/com/stance/interfaces/ProjectController.java
@@ -1,8 +1,11 @@
 package com.stance.interfaces;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/projects")
 public class ProjectController {
+
 
 }
